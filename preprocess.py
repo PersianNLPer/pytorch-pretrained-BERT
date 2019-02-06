227b257d96e402adb2c7a90604d96bcad96fb183
Amin Farajian <ma.farajian@gmail.com>
2019-02-06 19:00:58 +0000

    added preprocess script

---
+++ preprocess.py
@@ -0,0 +1,21 @@
+import argparse
+import sys
+import os
+from pytorch_pretrained_bert import BertTokenizer
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=argparse.FileType('r'), default=sys.stdin, metavar='PATH',
+                        help="The input file to be tokenized (default: STDIN")
+    parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdin, metavar='PATH',
+                        help="The output file containing the tokenized version of the input file (default: STDIN")
+    # parser.add_argument('-use_word_rep_source', action="store_true",
+    #                     help="Store word representation of the source as well.")
+
+    args = parser.parse_args()
+    tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=False)
+    for line in args.input:
+        args.output.write("%s\n" % (' '.join(tokenizer.tokenize(line.strip()))))
+
+    args.input.close()
+    args.output.close()
