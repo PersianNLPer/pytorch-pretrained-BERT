171d4b03991c25642a6124c24927cfa1d4ca69b5
Amin Farajian <ma.farajian@gmail.com>
2019-02-07 16:49:25 +0000

    just apply wordpiece tokenization

--- preprocess.py
+++ preprocess.py
@@ -1,24 +1,22 @@
 import argparse
 import sys
 import os
-from pytorch_pretrained_bert import BertTokenizer, BasicTokenizer
+from pytorch_pretrained_bert import BertTokenizer, BasicTokenizer, WordpieceTokenizer
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', type=argparse.FileType('r'), default=sys.stdin, metavar='PATH',
                         help="The input file to be tokenized (default: STDIN")
-    parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdin, metavar='PATH',
+    parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdout, metavar='PATH',
                         help="The output file containing the tokenized version of the input file (default: STDIN")
-    parser.add_argument('-segment', action="store_true",
-                        help="Segment the words after preprocessing?")
 
     args = parser.parse_args()
-    if args.segment:
-        tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=False)
-    else:
-        tokenizer = BasicTokenizer(do_lower_case=False)
+
+    tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=False)
+
     for line in args.input:
-        args.output.write("%s\n" % (' '.join(tokenizer.tokenize(line.strip()))))
+        args.output.write("%s\n" % (' '.join(tokenizer.wordpiece_tokenizer.tokenize(line.strip()))))
 
     args.input.close()
     args.output.close()
